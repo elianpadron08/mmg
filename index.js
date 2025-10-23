88b27196cce5e30e0d2c4cb7a3a7f0de152c9eb9
elianpadron08 <elianpadronhernandez@gmail.com>
2025-10-23 12:29:47 -0400

    acto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./db.js');
-const Usuario = require('./Backend/models/usuarioModels');
+const Usuario = require('./Backend/models/usuarioModels.js');
 require('dotenv').config();
 
 const app = express();
